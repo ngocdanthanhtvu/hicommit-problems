c5f8cd1ce9bc557a6d8cf749d6c3cbd01a297d55
ngocdanthanhtvu <103914827+ngocdanthanhtvu@users.noreply.github.com>
2026-09-01 23:46:32 +0700

    Submit code at 9/1/2026, 11:46:00 PM by HiCommit

--- main.c
+++ main.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 
 int main() {
-  int a, b;
-  printf("\nNhap so thu nhat: ");
-  scanf("%d", &a);
-  printf("\nNhap so thu hai: ");
-  scanf("%d", &b);
-
-  printf("%d", a+b);
+  
+  int a,b;
+  scanf("%d%d",&a,&b);
+  printf("%d",a+b);
   return 0;
 }
